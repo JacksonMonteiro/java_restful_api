e8b660d5b47912532106d505c2a2a402ccf6306e
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2024-01-11 17:44:01 -0300

    Criação da rota de update

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,5 +48,18 @@ public class ProductController {
 		}
 
 		return ResponseEntity.status(HttpStatus.OK).body(productOptional.get());
+	}
+
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+			@RequestBody @Valid ProductRecordDto productRecordDto) {
+		Optional<ProductModel> product = productRepository.findById(id);
+		if (product.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+		}
+
+		var productModel = product.get();
+		BeanUtils.copyProperties(productRecordDto, productModel);
+		return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
 	}
 }
