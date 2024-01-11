7cb15797b3c00bca62f96cf8e9fd4a3c3e2ed5bb
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2024-01-11 14:27:21 -0300

    Criação da classe de Controller e do Repository

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -15,7 +15,7 @@ import jakarta.persistence.Table;
 public class ProductModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
+	@Id	
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID idProduct;
 	private String name;
